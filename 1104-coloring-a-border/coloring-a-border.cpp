033e9ded42cdd577aaa38c9457280cb72ba20b98
riturajistooop-design <riturajistooop@gmail.com>
2026-08-25 00:56:33 +0530

    Time: 3 ms (29.57%) | Memory: 19.3 MB (5.82%) - LeetSync

--- 1104-coloring-a-border/coloring-a-border.cpp
+++ 1104-coloring-a-border/coloring-a-border.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     set<pair<int,int>> visited;
-    void colorBoundary(vector<vector<int>>& grid,int i,int j,int& color,int& clr,vector<vector<int>>& visit){
+    void colorBoundary(vector<vector<int>>& grid,int i,int j,int& color,int& clr,vector<vector<int>>& visit,int& m,int& n){
         if(visit[i][j]!=-1) return;
         visit[i][j] = 1;
-        int m = grid.size();
-        int n = grid[0].size();
         if(grid[i][j] != clr) return;
         if(visited.find({i,j})!=visited.end()) return;
         if(i==0 || i == m-1 || j==0 || j == n-1){
@@ -28,17 +26,17 @@ public:
             visited.insert({i,j});
             grid[i][j] = color;
         }
-        if(i!=0) colorBoundary(grid,i-1,j,color,clr,visit);
-        if(j!=0) colorBoundary(grid,i,j-1,color,clr,visit);
-        if(i!=m-1) colorBoundary(grid,i+1,j,color,clr,visit);
-        if(j!=n-1) colorBoundary(grid,i,j+1,color,clr,visit);
+        if(i!=0) colorBoundary(grid,i-1,j,color,clr,visit,m,n);
+        if(j!=0) colorBoundary(grid,i,j-1,color,clr,visit,m,n);
+        if(i!=m-1) colorBoundary(grid,i+1,j,color,clr,visit,m,n);
+        if(j!=n-1) colorBoundary(grid,i,j+1,color,clr,visit,m,n);
     }
     vector<vector<int>> colorBorder(vector<vector<int>>& grid, int i, int j, int color) {
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> visit(m,vector<int>(n,-1));
         int clr = grid[i][j];
-        colorBoundary(grid,i,j,color,clr,visit);
+        colorBoundary(grid,i,j,color,clr,visit,m,n);
         return grid;
     }
 };
