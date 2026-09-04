e5b1a90914ef5cc8060f6bde24fec7c36e4fde04
riturajistooop-design <riturajistooop@gmail.com>
2026-09-04 16:13:05 +0530

    Time: 4 ms (56.42%) | Memory: 18.7 MB (39.47%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
+        int n = pre.size();
+        unordered_map<int,vector<int>> map;
+        vector<int> count(numCourses);
+        vector<int> visited(numCourses,-1);
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            map[pre[i][1]].push_back(pre[i][0]);
+            count[pre[i][0]]++;
+        }
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(count[i]==0) {q.push(i); visited[i]=1;}
+        }
+        while(!q.empty()){
+            int temp = q.front();
+            q.pop();
+            ans.push_back(temp);
+            int s = map[temp].size();
+            for(int i=0;i<s;i++){
+                count[map[temp][i]]--;
+                if(count[map[temp][i]] == 0 && visited[map[temp][i]] == -1) {q.push(map[temp][i]); visited[map[temp][i]] = 1;}
+            }
+        }
+        if(ans.size() == numCourses) return ans;
+        return {};
+    }
+};
