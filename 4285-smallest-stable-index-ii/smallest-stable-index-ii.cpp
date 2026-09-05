2ccb7d029fd021ec55bf31d74779e1424fb547f4
riturajistooop-design <riturajistooop@gmail.com>
2026-09-05 13:27:19 +0530

    Time: 11 ms (57.63%) | Memory: 202.6 MB (85.02%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -1,21 +1,17 @@
 class Solution {
-public:
+public://21
     int firstStableIndex(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> mx(n);
-        vector<int> mn(n);
-        int temp = nums[0];
-        for(int i=0;i<n;i++){
-            temp = max(temp,nums[i]);
-            mx[i] = temp;
+        int n=nums.size();
+        vector<int> minv(n);
+        int mint=INT_MAX;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]<mint)mint=nums[i];
+            minv[i]=mint;
         }
-        temp = nums[n-1];
-        for(int i = n-1 ; i>=0 ; i-- ){
-            temp = min(temp,nums[i]);
-            mn[i] = temp;
-        }
+        int maxt=-1;
         for(int i=0;i<n;i++){
-            if((mx[i]-mn[i]) <= k) return i;
+            if(nums[i]>maxt)maxt=nums[i];
+            if(maxt-minv[i]<=k)return i;
         }
         return -1;
     }
