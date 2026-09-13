b3baaf15e846da9caf72c36c0fa753813ea29506
riturajistooop-design <riturajistooop@gmail.com>
2026-09-14 03:15:23 +0530

    Time: 4 ms (89.63%) | Memory: 18.9 MB (66.68%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,56 +1,21 @@
 class Solution {
 public:
-    // typedef pair<char,int> pci;
     int lengthOfLongestSubstring(string s) {
-        int n = s.length();
-        if(n==0) return n;
-        unordered_set<char> set;
-        int len = 1;
-        int ans = 1;
-        int i = 0;
-        int j = 1;
-        set.insert(s[i]);
-        while(j<n){
-            if(set.find(s[j])!=set.end()){
-                ans = max(ans,len);
-                while(s[i]!=s[j]){
-                    len--;
-                    set.erase(s[i]);
-                    i++;
-                }
-                i++;
-                j++;
+
+        int count[256]={0};
+        int l=0;
+        int r=0;
+        int ans=0;
+
+        while(r<s.length()){
+            count[s[r]]++;
+            while(count[s[r]]>1){
+                count[s[l]]--;
+                l++;
             }
-            else{
-                set.insert(s[j]);
-                j++;
-                len++;
-            }
+            ans = max(ans,r-l+1);
+            r++;
         }
-        ans = max(ans,len);
         return ans;
     }
 };
-/*
-int lengthOfLongestSubstring(string s) {
-        int n = s.length();
-        if(n==0) return n;
-        unordered_map<char,int> set;
-        int len = 0;
-        int ans = 1;
-        for(int i=0;i<n;i++){
-            if(set.find(s[i])!=set.end()){
-                ans = max(ans,len);
-                len = 0;
-                i = set[s[i]];
-                set.clear();
-            }
-            else{
-                set.insert({s[i],i});
-                len++;
-            }
-        }
-        ans = max(ans,len);
-        return ans;
-    }
-*/
