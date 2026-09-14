aef38598263378962f5995609542a91a1f707d77
riturajistooop-design <riturajistooop@gmail.com>
2026-09-15 02:22:05 +0530

    Time: 1 ms (48.50%) | Memory: 16.4 MB (82.13%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        int s = n.length();
-        int ans = 0;
-        for(int i=0;i<s;i++){
-            ans = max(ans,n[i]-'0');
-        }
-        return ans;
+        return *max_element(n.begin(), n.end()) - '0';
     }
 };
